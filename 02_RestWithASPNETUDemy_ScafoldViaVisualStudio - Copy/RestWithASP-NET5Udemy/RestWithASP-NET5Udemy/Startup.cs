1f733f4785484028a5579e00d5f4877765427eb9
Cleytonff <cleytonff@gmail.com>
2021-02-18 01:36:46 -0300

    Seção 11, Migration

--- 02_RestWithASPNETUDemy_ScafoldViaVisualStudio - Copy/RestWithASP-NET5Udemy/RestWithASP-NET5Udemy/Startup.cs
+++ 02_RestWithASPNETUDemy_ScafoldViaVisualStudio - Copy/RestWithASP-NET5Udemy/RestWithASP-NET5Udemy/Startup.cs
@@ -9,17 +9,28 @@ using RestWithASP_NET5Udemy.Business.Implementations;
 using RestWithASP_NET5Udemy.Model.Context;
 using RestWithASP_NET5Udemy.Repository;
 using RestWithASP_NET5Udemy.Repository.Implementations;
+using Serilog;
+using System;
+using System.Collections.Generic;
 
 namespace RestWithASP_NET5Udemy
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
         {
             Configuration = configuration;
+            Environment = environment;
+
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
         }
 
-        public IConfiguration Configuration { get; }
+        
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -30,11 +41,18 @@ namespace RestWithASP_NET5Udemy
 
             services.AddDbContext<MySQLContext>(options => options.UseMySql(connection));
 
+            if (Environment.IsDevelopment())
+            {
+                MigrateDatabase(connection);
+            }
+
             services.AddApiVersioning();
 
             services.AddScoped<IPersonBusiness, PersonBusinessImplementation>();
             services.AddScoped<IPersonRepository, PersonRepositoryImplementation>();
         }
+
+       
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -54,6 +72,27 @@ namespace RestWithASP_NET5Udemy
             {
                 endpoints.MapControllers();
             });
+        }
+
+        private void MigrateDatabase(string connection)
+        {
+            try
+            {
+                var evolveConnection = new MySql.Data.MySqlClient.MySqlConnection(connection);
+                var evolve = new Evolve.Evolve(evolveConnection, msg => Log.Information(msg))
+                {
+                    Locations = new List<string> { "db/migrations", "db/dataset" },
+                    IsEraseDisabled = true,
+
+                };
+
+                evolve.Migrate();
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Database migration failed", ex);
+;                throw;
+            }
         }
     }
 }
