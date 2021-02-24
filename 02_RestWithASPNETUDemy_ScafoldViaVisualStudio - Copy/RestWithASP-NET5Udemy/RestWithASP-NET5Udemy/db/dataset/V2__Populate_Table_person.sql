17255622b4c30edc2aa6fe540e2506d18ab54b59
Cleytonff <cleytonff@gmail.com>
2021-02-24 02:20:28 -0300

    Delete 02_RestWithASPNETUDemy_ScafoldViaVisualStudio - Copy/RestWithASP-NET5Udemy directory

--- 02_RestWithASPNETUDemy_ScafoldViaVisualStudio - Copy/RestWithASP-NET5Udemy/RestWithASP-NET5Udemy/db/dataset/V2__Populate_Table_person.sql
+++
@@ -1,5 +0,0 @@
-﻿INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
-	(1, 'Sao Paulo', 'Luciana', 'Female', 'Franco'),
-	(2, 'Porbandar - India', 'Mahatma', 'Female', 'Gandhi'),
-	(3, 'Kentuch - USA', 'Mohamedi', 'Male', 'Ali'),
-	(4, 'Sao Paulo - Brasil', 'Lucas', 'Male', 'Romao');
